c5c64dcf51394b8a7079a3981ea894541402fc89
fumiama <s862105088@gmail.com>
2021-08-03 22:51:05 +0800

    优化代码

--- classify.go
+++ classify.go
@@ -1,12 +1,14 @@
 package ACGImageClassify
 
 import (
+	"crypto/md5"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	zero "github.com/wdvxdr1123/ZeroBot"
@@ -103,17 +105,46 @@ func replyClass(ctx *zero.Ctx, dhash string, class int, noimg bool, lv int64) {
 				ctx.Send(comments[class] + "给你点提示哦:" + b14)
 			}
 			ctx.Event.GroupID = 0
-			ctx.SendGroupMessage(0, img)
-			ctx.SendChain(message.Text("偷偷发给你啦，不要和别人说哦"), img)
+			ctx.Send(message.Text("偷偷发给你啦，不要和别人说哦"))
+			ctx.SendGroupMessage(0, img.Add("cache", "1"))
+			ctx.Send(bigPic(cache_file+strconv.FormatInt(lv, 10), comments[class]))
 		} else {
 			ctx.Send(comments[class])
 		}
 	} else {
 		comment := message.Text(comments[class])
 		if !noimg {
-			ctx.SendChain(message.Image(cache_uri+strconv.FormatInt(lv, 10)), comment)
+			ctx.SendChain(img, comment)
 		} else {
 			ctx.SendChain(message.Reply(ctx.Event.MessageID), comment)
 		}
 	}
+}
+
+// PicHash 返回图片的 md5 值
+func picHash(path string) string {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return ""
+	}
+	return strings.ToUpper(fmt.Sprintf("%x", md5.Sum(data)))
+}
+
+// BigPic 返回一张XML大图CQ码
+func bigPic(file string, comment string) string {
+	var hash = picHash(file)
+	return fmt.Sprintf(`[CQ:xml,data=<?xml version='1.0' 
+encoding='UTF-8' standalone='yes' ?><msg serviceID="5" 
+templateID="12345" action="" brief="不够涩！" 
+sourceMsgId="0" url="" flag="0" adverSign="0" multiMsgFlag="0">
+<item layout="0" advertiser_id="0" aid="0"><image uuid="%s.jpg" md5="%s" 
+GroupFiledid="2235033681" filesize="81322" local_path="%s.jpg" 
+minWidth="200" minHeight="200" maxWidth="500" maxHeight="1000" />
+</item><source name="%s" icon="" 
+action="" appid="-1" /></msg>]`,
+		hash,
+		hash,
+		hash,
+		comment,
+	)
 }
